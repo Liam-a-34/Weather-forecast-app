3aac2421c870ac0a8c952e1d7030cfddd6a27191
Liam-a-34 <liamallen343@gmail.com>
2022-11-16 10:50:04 -0600

    Added comments to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const APIkey = "4d32bfe2b7f2d69ea5aa474a9f55742c"
-
+// Variable for current day in 01/01/2022 format
 const day = dayjs().format("MM/DD/YYYY")
 
 let visibility;
-
+// If there are items in the local storage "searches" it will append them in the search history on page load
 if(localStorage.getItem("searches") != null){
 
     let searchHistory = localStorage.getItem("searches")
@@ -13,25 +13,24 @@ if(localStorage.getItem("searches") != null){
     console.log(searchHistory)
 
 }
-
+// Main function
 function setWeather(city){
-
+// Current day weather URL
 let weatherURLToday = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`
+// Five day forecast URL
 let weatherURLForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=imperial`
-
+// Clears the current weather and forecast weather so nothing collides
     $("#currentWeather").empty();
     $("#forecasts").empty();
-
+// Fetches the current day weather data and parses it
 fetch(weatherURLToday)
 .then(function (response) {
     return response.json()
-})
+})//Passes the response in to the function, as data
 .then(function (data) {
     console.log(data)
 
-    let temperature = data.main.temp
-    let windSpeed = data.wind.speed
-    let humidity = data.main.humidity
+// Checks to see what the weather is and assigns the correct icon
     if(data.weather[0].main == "Clear"){
         if(dayjs().format("HH") < 6 || dayjs().format("HH") > 8){
             visibility = "./assets/images/clipart3046907.png"
@@ -51,13 +50,13 @@ fetch(weatherURLToday)
         visibility = "./assets/images/icons8-thunderstorm-60.png"
     }
 
-
+// Creates the search history button element
     let cityNameButton =`<button class="cityBtn" value="${data.name}">${data.name}</button>`
-
+// Appends the button
         $("#citySearches").append(cityNameButton);
 
         let allSearches = []
-
+// If localStorage "searches" has content it will be executed differently than if it is empty. TO avoid a null entry
         if(localStorage.getItem("searches") != null){
 
             allSearches = localStorage.getItem("searches")
@@ -80,14 +79,14 @@ fetch(weatherURLToday)
 
  
     
-
+// Current weather information element
     let todaysInfo =
 
     `
     <h2>${data.name} <span>${day}</span> <img src="${visibility}"></h2>
-        <p>Temp: ${temperature} °F</p>
-        <p>Wind: ${windSpeed} MPH</p>
-        <p>Humidity: ${humidity}%</p>
+        <p>Temp: ${data.main.temp} °F</p>
+        <p>Wind: ${data.wind.speed} MPH</p>
+        <p>Humidity: ${data.main.humidity}%</p>
     `
     $("#currentWeather").append(todaysInfo)
 
@@ -95,7 +94,7 @@ fetch(weatherURLToday)
     visibility = "";
 
 })
-
+// Fetches the five day forecast and handles it like the same day weather
 fetch(weatherURLForecast)
 .then(function (response) {
     return response.json();
@@ -105,8 +104,9 @@ fetch(weatherURLForecast)
 
 
     let dayCount = 1;
-
+// Loops through at a specific interval, to get the weather for every day at 12 noon.
 for (var i = 4; i < 37; i += 8){
+    // Day formatted so that we can use the daycount variable to add another day every loop, and give us five days in a row
     let forecastDay = dayjs().add(dayCount, "day").format("MM/DD/YYYY")
 
     if(data.list[i].weather[0].main == "Clear"){
@@ -123,7 +123,7 @@ for (var i = 4; i < 37; i += 8){
      else {
         visibility = "./assets/images/icons8-thunderstorm-60.png"
     }
-
+// Single forecast card element
     let forecastInfo = 
     `
     <div class="forecastCard">
@@ -134,31 +134,32 @@ for (var i = 4; i < 37; i += 8){
             <p>Humidity: ${data.list[i].main.humidity}%</p>
     </div>
     `
+    // Appends the card
     $("#forecasts").append(forecastInfo)
-
+// Adds a day to the 
     dayCount += 1;
     visibility = "";
 }
 
 })
-
+// Clears the input field
     $("#cityInput").val("")
 }
 
-
+// Runs the setWeather function with the value of the buttons city name, passing it in to the function
 $(".cityBtn").on("click", function(){
     city = $(this).attr("value");
     
     setWeather(city)
 })
 
-
+// Runs the setWeather function, with the input field value passed in to the function
 $("#searchBtn").on("click", function(){
     city = $("#cityInput").val();
 
     setWeather(city)
 })
-
+// Clears the "searches" local storage and the search history div
 $(".clearButton").on("click", function(){
     localStorage.clear("searches")
     $("#citySearches").empty()
